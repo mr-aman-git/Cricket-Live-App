c02c8bf97a49a3b16c0d89d32e4f348e665a90ac
mr-aman-git <amanazr2003@gmail.com>
2025-03-03 21:13:55 +0530

    custom hook create

--- src/Components/LiveMatches.jsx
+++ src/Components/LiveMatches.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import useCustomHook from "../CustomHook";
+
 
 let MatchUrl =
   "https://api.cricapi.com/v1/cricScore?apikey=ed41540b-edfa-4c13-95a7-f58d0200cecb";
 const LiveMatches = () => {
-  let [apidata, setApidata] = useState([]);
+  
+let {setApidata, apidata} = useCustomHook();
 
   useEffect(() => {
     let Upcomming = async () => {
@@ -90,30 +93,4 @@ const LiveMatches = () => {
 
 export default LiveMatches;
 
-{
-  /* <div className='bg-white w-[350px] rounded-md mt-2' key={store.id}>
-                <h3 className='text-center font-medium  text-blue-700 '> {store.series} </h3>
-                <p className='text-center text-[14px] '> {store.ms} </p>
-                        <hr className='text-orange-500' />
-                <div className='flex'>
-                    <div className='w-[200px] p-2'> 
-                        <div className='flex gap-2'>
-                            <img src= {store.t1img} alt="flag" className='w-[30px] rounded-[50%]' />
-                            <h4 className='text-[14px]'> {store.t1}</h4>
-                        </div>
 
-                        <div className='flex mt-3 gap-2'>
-                            <img src= {store.t2img} alt="flag" className='w-[30px] rounded-[50%]' />
-                            <h4 className='text-[14px]'>{store.t2}</h4>
-                        </div>
-                        
-                    </div>
-
-                    <div className=' p-2'>
-                        <p className='text-[14px]'>{store.dateTimeGMT}</p>
-                        <p> {store.matchType} </p>
-                    </div>
-                </div>
-                 <p className='text-center text-green-700 font-medium'> {store.status} </p>
-            </div> */
-}
